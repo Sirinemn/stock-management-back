a6ae5b1af2811b808fdb05a0110c6e9caceb7f70
Sirinemn <sirinemn@outlook.fr>
2025-02-19 20:55:13 +0100

    implements StockMovement entity

--- src/main/java/fr/sirine/stock_management_back/entities/StockMovement.java
+++ src/main/java/fr/sirine/stock_management_back/entities/StockMovement.java
@@ -1,4 +1,36 @@
 package fr.sirine.stock_management_back.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "_stock_movements")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StockMovement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Enumerated(EnumType.STRING)
+    private TypeMovement type;
+
+    private int quantity;
+    private LocalDateTime date = LocalDateTime.now();
+    enum TypeMovement {
+        ENTREE, SORTIE
+    }
+
 }
