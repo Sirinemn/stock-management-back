69af5e50fced03a7b97e2edff27d2e9c73c1c73c
Sirinemn <sirinemn@outlook.fr>
2025-02-19 20:58:11 +0100

    implements UserRepository

---
+++ src/main/java/fr/sirine/stock_management_back/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package fr.sirine.stock_management_back.repository;
+
+import java.util.Optional;
+import fr.sirine.stock_management_back.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
