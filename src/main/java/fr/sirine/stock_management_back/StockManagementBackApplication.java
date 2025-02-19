f1f4ba92976801a552b777db85a2a1e986c6a9f0
Sirinemn <sirinemn@outlook.fr>
2025-02-19 17:22:57 +0100

    first commit

---
+++ src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
@@ -0,0 +1,13 @@
+package fr.sirine.stock_management_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/sirine/stock_management_back/StockManagementBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sirine.stock_management_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
