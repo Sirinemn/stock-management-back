5d508e9c61c74ef2b8d04688a375922837eccda0
Sirinemn <sirinemn@outlook.fr>
2025-02-19 20:53:11 +0100

    implements product entity

--- src/main/java/fr/sirine/stock_management_back/entities/Product.java
+++ src/main/java/fr/sirine/stock_management_back/entities/Product.java
@@ -1,4 +1,28 @@
 package fr.sirine.stock_management_back.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "_products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String description;
+    private int quantity;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
