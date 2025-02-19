dd60017713c90a56bcbd67b9ffb348858785c330
Sirinemn <sirinemn@outlook.fr>
2025-02-19 21:01:15 +0100

    implements ProductRepositoryRepository

---
+++ src/main/java/fr/sirine/stock_management_back/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package fr.sirine.stock_management_back.repository;
+
+import fr.sirine.stock_management_back.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+    List<Product> findByUserId(Long userId);
+}
