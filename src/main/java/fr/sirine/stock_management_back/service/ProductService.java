ead18f6abd39378af09fdc4032ef432571bdae8c
Sirinemn <sirinemn@outlook.fr>
2025-02-21 15:58:14 +0100

    add ProductService

---
+++ src/main/java/fr/sirine/stock_management_back/service/ProductService.java
@@ -0,0 +1,18 @@
+package fr.sirine.stock_management_back.service;
+
+import fr.sirine.stock_management_back.entities.Product;
+import fr.sirine.stock_management_back.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product findProductById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+}
