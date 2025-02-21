6bc27c83e834b2f60bf78af0a6ba92733c7c9a62
Sirinemn <sirinemn@outlook.fr>
2025-02-21 15:01:58 +0100

    add UserDto

---
+++ src/main/java/fr/sirine/stock_management_back/dto/UserDto.java
@@ -0,0 +1,22 @@
+package fr.sirine.stock_management_back.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private LocalDateTime dateOfBirth;
+    private String email;
+    private LocalDateTime createdDate;
+    private LocalDateTime lastModifiedDate;
+    private List<String> roles;
+}
