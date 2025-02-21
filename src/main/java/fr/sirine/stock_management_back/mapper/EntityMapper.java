e338bcc7b336ccf6dd0ebfbd49a5080d13fed325
Sirinemn <sirinemn@outlook.fr>
2025-02-21 15:06:20 +0100

    add EntityMapper interface

---
+++ src/main/java/fr/sirine/stock_management_back/mapper/EntityMapper.java
@@ -0,0 +1,13 @@
+package fr.sirine.stock_management_back.mapper;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface EntityMapper<D, E> {
+
+    D toDto(E entity) throws IOException;
+
+    List<E> toEntity(List<D> dtoList);
+
+    List<D> toDto(List<E> entityList);
+}
