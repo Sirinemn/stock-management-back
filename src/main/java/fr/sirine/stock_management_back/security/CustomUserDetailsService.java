337ba4f648ab917c50b2c6fe1ee95640927b129f
Sirinemn <sirinemn@outlook.fr>
2025-02-24 18:53:22 +0100

    add CustomUserDetailsService

---
+++ src/main/java/fr/sirine/stock_management_back/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package fr.sirine.stock_management_back.security;
+
+import fr.sirine.stock_management_back.entities.User;
+import fr.sirine.stock_management_back.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        User user =this.userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found : "+ userEmail));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getAuthorities()
+        );
+    }
+}
