b7792dd7e0722b2ed49ea399797f2755a83d3729
Sirinemn <sirinemn@outlook.fr>
2025-02-25 17:22:57 +0100

    add User role in Role Table

--- src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
+++ src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
@@ -1,7 +1,11 @@
 package fr.sirine.stock_management_back;
 
+import fr.sirine.stock_management_back.entities.Role;
+import fr.sirine.stock_management_back.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StockManagementBackApplication {
@@ -9,5 +13,12 @@ public class StockManagementBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockManagementBackApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
