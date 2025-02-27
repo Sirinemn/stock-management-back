4b5610a4c26e515605fcdd11121412cea4d14348
Sirinemn <sirinemn@outlook.fr>
2025-02-27 10:23:45 +0100

    add roles to AuthenticationResponse and update AuthenticationService

--- src/main/java/fr/sirine/stock_management_back/auth/AuthenticationService.java
+++ src/main/java/fr/sirine/stock_management_back/auth/AuthenticationService.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
