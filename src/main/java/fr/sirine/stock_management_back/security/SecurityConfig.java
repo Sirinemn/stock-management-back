6e834efe43e68152f0a75da214fe0045489b6109
Sirinemn <sirinemn@outlook.fr>
2025-02-28 09:57:53 +0100

    Disable security for swagger

--- src/main/java/fr/sirine/stock_management_back/security/SecurityConfig.java
+++ src/main/java/fr/sirine/stock_management_back/security/SecurityConfig.java
@@ -29,7 +29,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .authenticationProvider(authenticationProvider)
