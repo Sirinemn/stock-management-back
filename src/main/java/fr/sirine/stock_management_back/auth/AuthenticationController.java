5e4d6472fc9078ec7ae5991c32ba0dbea2f2bce0
Sirinemn <sirinemn@outlook.fr>
2025-02-28 10:58:41 +0100

    add swagger documentation swagger to my AuthenticationController

--- src/main/java/fr/sirine/stock_management_back/auth/AuthenticationController.java
+++ src/main/java/fr/sirine/stock_management_back/auth/AuthenticationController.java
@@ -3,6 +3,12 @@ package fr.sirine.stock_management_back.auth;
 import fr.sirine.stock_management_back.payload.request.LoginRequest;
 import fr.sirine.stock_management_back.payload.request.RegisterRequest;
 import fr.sirine.stock_management_back.payload.response.AuthenticationResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name="Authentication")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -21,11 +28,24 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    @Operation(summary = "Authentification utilisateur", description = "Permet à un utilisateur de se connecter avec ses identifiants")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Authentification réussie",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AuthenticationResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Requête invalide"),
+            @ApiResponse(responseCode = "401", description = "Authentification échouée")
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
         return  ResponseEntity.ok(authenticationService.authenticate(loginRequest));
     }
 
+    @Operation(summary = "Inscription utilisateur", description = "Permet à un utilisateur de s'inscrire")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", description = "Inscription réussie"),
+            @ApiResponse(responseCode = "400", description = "Requête invalide")
+    })
     @PostMapping("/register")
     private ResponseEntity<?> register(@RequestBody @Valid RegisterRequest registerRequest) {
         authenticationService.register(registerRequest);
