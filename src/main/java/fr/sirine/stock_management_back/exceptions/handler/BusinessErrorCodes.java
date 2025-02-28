b1c2c7993a8c982369c5b6e1e9e7928d4876f15f
Sirinemn <sirinemn@outlook.fr>
2025-02-28 15:52:49 +0100

    add jwt_token_expired to BusinessErrorCodes enum

--- src/main/java/fr/sirine/stock_management_back/exceptions/handler/BusinessErrorCodes.java
+++ src/main/java/fr/sirine/stock_management_back/exceptions/handler/BusinessErrorCodes.java
@@ -10,7 +10,9 @@ public enum BusinessErrorCodes {
     EMAIL_ALREADY_USED(1002, "Email déjà utilisé", HttpStatus.CONFLICT),
     UNAUTHORIZED_ACTION(1003, "Action non autorisée", HttpStatus.FORBIDDEN),
     INVALID_REQUEST(1004, "Requête invalide", HttpStatus.BAD_REQUEST),
-    INTERNAL_SERVER_ERROR(1005, "Erreur interne du serveur", HttpStatus.INTERNAL_SERVER_ERROR);
+    INTERNAL_SERVER_ERROR(1005, "Erreur interne du serveur", HttpStatus.INTERNAL_SERVER_ERROR),
+    JWT_TOKEN_EXPIRED(1006, "Token JWT expiré", HttpStatus.UNAUTHORIZED);
+
 
     private final int code;
     private final String description;
