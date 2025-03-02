b2a97e5d4b9fefdb86553dfccc8d4833a7eacdf4
Sirinemn <sirinemn@outlook.fr>
2025-03-02 12:33:21 +0100

    update glabalExceptionHandler

--- src/main/java/fr/sirine/stock_management_back/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/fr/sirine/stock_management_back/exceptions/handler/GlobalExceptionHandler.java
@@ -8,43 +8,44 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static fr.sirine.stock_management_back.exceptions.handler.BusinessErrorCodes.*;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EmailAlreadyUsedException.class)
-    public ResponseEntity<ExceptionResponse> handleException(EmailAlreadyUsedException exp) {
+    private ResponseEntity<ExceptionResponse> buildResponseEntity(BusinessErrorCodes errorCode, String message) {
         return ResponseEntity
-                .status(EMAIL_ALREADY_USED.getHttpStatus())
+                .status(errorCode.getHttpStatus())
                 .body(
                         ExceptionResponse.builder()
-                                .errorCode(EMAIL_ALREADY_USED.getCode())
-                                .message(exp.getMessage())
-                                .httpStatus(EMAIL_ALREADY_USED.getHttpStatus().value())
+                                .errorCode(errorCode.getCode())
+                                .message(message)
+                                .httpStatus(errorCode.getHttpStatus().value())
                                 .build()
                 );
+    }
+
+    @ExceptionHandler(EmailAlreadyUsedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(EmailAlreadyUsedException exp) {
+        return buildResponseEntity(BusinessErrorCodes.EMAIL_ALREADY_USED, exp.getMessage());
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleException(UserNotFoundException exp) {
-        return ResponseEntity
-                .status(USER_NOT_FOUND.getHttpStatus())
-                .body(
-                        ExceptionResponse.builder()
-                                .errorCode(USER_NOT_FOUND.getCode())
-                                .message(exp.getMessage())
-                                .httpStatus(USER_NOT_FOUND.getHttpStatus().value())
-                                .build()
-                );
+        return buildResponseEntity(BusinessErrorCodes.USER_NOT_FOUND, exp.getMessage());
     }
     @ExceptionHandler(JwtTokenExpiredException.class)
     public ResponseEntity<ExceptionResponse> handleException(JwtTokenExpiredException exp) {
+        return buildResponseEntity(BusinessErrorCodes.JWT_TOKEN_EXPIRED, exp.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception exp) {
+        exp.printStackTrace();
         return ResponseEntity
-                .status(JWT_TOKEN_EXPIRED.getHttpStatus())
+                .status(INTERNAL_SERVER_ERROR)
                 .body(
                         ExceptionResponse.builder()
-                                .errorCode(JWT_TOKEN_EXPIRED.getCode())
-                                .message(exp.getMessage())
-                                .httpStatus(JWT_TOKEN_EXPIRED.getHttpStatus().value())
+                                .message("Internal error, please contact the admin")
+                                .errorCode(INTERNAL_SERVER_ERROR.value())
                                 .build()
                 );
     }
