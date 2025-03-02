47f7eacf2538e8be1a4c2fec179368e2335503a3
Sirinemn <sirinemn@outlook.fr>
2025-03-02 15:18:27 +0100

    add builder to Product entity

--- src/main/java/fr/sirine/stock_management_back/entities/Product.java
+++ src/main/java/fr/sirine/stock_management_back/entities/Product.java
@@ -1,15 +1,13 @@
 package fr.sirine.stock_management_back.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "_products")
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
