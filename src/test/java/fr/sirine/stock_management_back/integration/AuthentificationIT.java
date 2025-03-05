ceb7e94109bac64c089426117568cabb82149e9f
Sirinemn <sirinemn@outlook.fr>
2025-03-05 10:22:34 +0100

    update AuthentificationIT tests

--- src/test/java/fr/sirine/stock_management_back/integration/AuthentificationIT.java
+++ src/test/java/fr/sirine/stock_management_back/integration/AuthentificationIT.java
@@ -1,9 +1,12 @@
 package fr.sirine.stock_management_back.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.sirine.stock_management_back.StockManagementBackApplication;
 import fr.sirine.stock_management_back.auth.AuthenticationService;
+import fr.sirine.stock_management_back.payload.request.LoginRequest;
 import fr.sirine.stock_management_back.payload.request.RegisterRequest;
+import fr.sirine.stock_management_back.payload.response.AuthenticationResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,7 +20,11 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = StockManagementBackApplication.class)
@@ -49,5 +56,28 @@ public class AuthentificationIT {
                 .content(registrationRequestJson);
         mockMvc.perform(request)
                 .andExpect(status().isAccepted());
+    }
+    @Test
+    void shouldLoginSuccessfully() throws Exception {
+        LoginRequest loginRequest = LoginRequest.builder()
+                .email("email@test.fr")
+                .password("password")
+                .build();
+        AuthenticationResponse authResponse = AuthenticationResponse.builder()
+                .token("dummy-jwt-token")
+                .userId(1)
+                .roles(List.of("ROLE_USER")) // Assuming roles are returned like this
+                .build();
+        when(authenticationService.authenticate(any())).thenReturn(authResponse);
+
+        String loginRequestJson = objectMapper.writeValueAsString(loginRequest);
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(loginRequestJson);
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.token").value("dummy-jwt-token"))
+                .andExpect(jsonPath("$.userId").value(1));
+
     }
 }
