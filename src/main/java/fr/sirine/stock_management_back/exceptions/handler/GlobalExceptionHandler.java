8d6307e5aa01724b8626da680731cc89c09ed82e
Sirinemn <sirinemn@outlook.fr>
2025-03-05 10:02:43 +0100

    add MethodArgumentNotValidException handler

--- src/main/java/fr/sirine/stock_management_back/exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/fr/sirine/stock_management_back/exceptions/handler/GlobalExceptionHandler.java
@@ -5,10 +5,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,39 +31,48 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyUsedException.class)
     public ResponseEntity<ExceptionResponse> handleException(EmailAlreadyUsedException exp) {
+        logger.error("Email already used", exp);
         return buildResponseEntity(BusinessErrorCodes.EMAIL_ALREADY_USED, exp.getMessage());
     }
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ExceptionResponse> handleException() {
+        logger.error("Bad credentials");
         return buildResponseEntity(BusinessErrorCodes.BAD_CREDENTIALS, "Login and / or Password is incorrect");
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException ex) {
+        logger.error("Validation error", ex);
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        String errorMessage = errors.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .collect(Collectors.joining(", "));
+        return buildResponseEntity(BusinessErrorCodes.INVALID_REQUEST, errorMessage);
     }
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleException(UserNotFoundException exp) {
+        logger.error("User not found", exp);
         return buildResponseEntity(BusinessErrorCodes.USER_NOT_FOUND, exp.getMessage());
     }
     @ExceptionHandler(JwtTokenExpiredException.class)
     public ResponseEntity<ExceptionResponse> handleException(JwtTokenExpiredException exp) {
+        logger.error("JWT token expired", exp);
         return buildResponseEntity(BusinessErrorCodes.JWT_TOKEN_EXPIRED, exp.getMessage());
     }
     @ExceptionHandler(UnauthorizedActionException.class)
     public ResponseEntity<ExceptionResponse> handleException(UnauthorizedActionException exp) {
+        logger.error("Unauthorized action", exp);
         return buildResponseEntity(BusinessErrorCodes.UNAUTHORIZED_ACTION, exp.getMessage());
     }
     @ExceptionHandler(RoleNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleException(RoleNotFoundException exp) {
+        logger.error("Role not found", exp);
         return buildResponseEntity(BusinessErrorCodes.ROLE_NOT_FOUND, exp.getMessage());
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception exp) {
         logger.error("An unexpected error occurred", exp);
-        return ResponseEntity
-                .status(INTERNAL_SERVER_ERROR)
-                .body(
-                        ExceptionResponse.builder()
-                                .message("Internal error, please contact the admin")
-                                .errorCode(INTERNAL_SERVER_ERROR.value())
-                                .httpStatus(INTERNAL_SERVER_ERROR.value())
-                                .build()
-                );
+        return buildResponseEntity(BusinessErrorCodes.INTERNAL_SERVER_ERROR, "Internal error, please contact the admin");
     }
 }
