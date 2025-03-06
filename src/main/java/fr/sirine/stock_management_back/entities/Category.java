1a41cee5dad3efcc68aacdd9872a553db3837fb6
Sirinemn <sirinemn@outlook.fr>
2025-03-06 15:47:58 +0100

    add Category entity

---
+++ src/main/java/fr/sirine/stock_management_back/entities/Category.java
@@ -0,0 +1,46 @@
+package fr.sirine.stock_management_back.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "_categories")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+}
+
