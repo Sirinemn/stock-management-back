c4151676e6abd7fc9a3742188618114c19af93d0
Sirinemn <sirinemn@outlook.fr>
2025-03-06 10:45:43 +0100

    update UserServiceTest

--- src/test/java/fr/sirine/stock_management_back/service/UserServiceTest.java
+++ src/test/java/fr/sirine/stock_management_back/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package fr.sirine.stock_management_back.service;
 
 import fr.sirine.stock_management_back.entities.User;
 import fr.sirine.stock_management_back.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,13 +22,18 @@ public class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .id(1)
+                .email("john@oefr")
+                .build();
+    }
     @Test
     void should_find_user_by_id() {
-        User initialUser = User.builder()
-                .id(1)
-                .email("john@doe.fr")
-                .build();
-        when(userRepository.findById(1)).thenReturn(Optional.ofNullable(initialUser));
+        when(userRepository.findById(1)).thenReturn(Optional.ofNullable(user));
         userService.findById(1);
         verify(userRepository, times(1)).findById(1);
     }
