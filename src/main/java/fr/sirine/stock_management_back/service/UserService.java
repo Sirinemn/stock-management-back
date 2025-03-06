2cfff44682c1face21d6e31f6bb18ea9fe1122b3
Sirinemn <sirinemn@outlook.fr>
2025-03-06 10:18:41 +0100

    add getAllUsers method to UserService

--- src/main/java/fr/sirine/stock_management_back/service/UserService.java
+++ src/main/java/fr/sirine/stock_management_back/service/UserService.java
@@ -1,18 +1,35 @@
 package fr.sirine.stock_management_back.service;
 
+import fr.sirine.stock_management_back.dto.UserDto;
 import fr.sirine.stock_management_back.entities.User;
+import fr.sirine.stock_management_back.mapper.UserMapper;
 import fr.sirine.stock_management_back.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     public User findById(Integer id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    public List<UserDto> getAllUsers() {
+        return userRepository.findAll().stream()
+                .filter(user -> user.getRoles().stream().noneMatch(role -> role.getName().equals("ADMIN")))
+                .map(userMapper::toDto)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
     }
 }
