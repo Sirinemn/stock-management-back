cfc93682561931941625b3c2e87c36974e75127b
Sirinemn <sirinemn@outlook.fr>
2025-03-06 15:48:39 +0100

    add ManyToOne relation with Category entity

--- src/main/java/fr/sirine/stock_management_back/entities/Product.java
+++ src/main/java/fr/sirine/stock_management_back/entities/Product.java
@@ -23,4 +23,9 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
 }
