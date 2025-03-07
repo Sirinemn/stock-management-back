25a82ff6c329d81bfecd47b194d9881bb29619e5
Sirinemn <sirinemn@outlook.fr>
2025-03-07 10:34:51 +0100

    initialise admin user in main

--- src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
+++ src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
@@ -1,12 +1,19 @@
 package fr.sirine.stock_management_back;
 
 import fr.sirine.stock_management_back.entities.Role;
+import fr.sirine.stock_management_back.entities.User;
 import fr.sirine.stock_management_back.repository.RoleRepository;
+import fr.sirine.stock_management_back.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -15,11 +22,28 @@ public class StockManagementBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockManagementBackApplication.class, args);
 	}
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	@Bean
-	public CommandLineRunner runner(RoleRepository roleRepository) {
+	public CommandLineRunner runner(RoleRepository roleRepository, UserRepository userRepository) {
 		return args -> {
 			if (roleRepository.findByName("USER").isEmpty()) {
 				roleRepository.save(Role.builder().name("USER").build());
+			}
+			if (roleRepository.findByName("ADMIN").isEmpty()) {
+				roleRepository.save(Role.builder().name("ADMIN").build());
+			}
+			// Initialiser l'utilisateur admin
+			if (userRepository.findByEmail("admin@mail.fr").isEmpty()) {
+				User admin = new User();
+				admin.setFirstname("admin");
+				admin.setEmail("admin@mail.fr");
+				admin.setLastname("Last");
+				admin.setPassword(passwordEncoder().encode("adminpassword"));
+				Role adminRole = roleRepository.findByName("ADMIN").orElseThrow(() -> new RuntimeException("Role ADMIN not found"));
+				admin.setRoles(new ArrayList<>(Collections.singleton(adminRole)));
+				userRepository.save(admin);
 			}
 		};
 	}
