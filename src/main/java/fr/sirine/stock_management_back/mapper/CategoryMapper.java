e9901a3599221aa726f701223cee5eb14664deb7
Sirinemn <sirinemn@outlook.fr>
2025-03-11 10:00:43 +0100

    fix CategoryMapper

--- src/main/java/fr/sirine/stock_management_back/mapper/CategoryMapper.java
+++ src/main/java/fr/sirine/stock_management_back/mapper/CategoryMapper.java
@@ -3,6 +3,7 @@ package fr.sirine.stock_management_back.mapper;
 import fr.sirine.stock_management_back.dto.CategoryDto;
 import fr.sirine.stock_management_back.entities.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -10,15 +11,13 @@ import java.io.IOException;
 @Component
 @Mapper(componentModel = "spring")
 public abstract class CategoryMapper implements EntityMapper<CategoryDto, Category>{
-    @Override
-    public CategoryDto toDto(Category entity) {
-        return null;
-    }
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    public abstract CategoryDto toDto(Category entity);
 
     @Override
-    public Category toEntity(CategoryDto dto) {
-        return null;
-    }
-
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    public abstract Category toEntity(CategoryDto dto);
 
 }
