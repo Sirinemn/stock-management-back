d150ac8e14d1b3940dc6af44809e8ca912be03b0
Sirinemn <sirinemn@outlook.fr>
2025-03-11 22:40:55 +0100

    implement ProductServiceTest

--- src/test/java/fr/sirine/stock_management_back/service/ProductServiceTest.java
+++ src/test/java/fr/sirine/stock_management_back/service/ProductServiceTest.java
@@ -1,9 +1,14 @@
 package fr.sirine.stock_management_back.service;
 
 
+import fr.sirine.stock_management_back.dto.ProductDto;
+import fr.sirine.stock_management_back.entities.Category;
 import fr.sirine.stock_management_back.entities.Product;
+import fr.sirine.stock_management_back.entities.User;
+import fr.sirine.stock_management_back.mapper.ProductMapper;
 import fr.sirine.stock_management_back.repository.ProductRepository;
 import fr.sirine.stock_management_back.service.impl.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +17,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,17 +27,71 @@ public class ProductServiceTest {
     private ProductService productService;
     @Mock
     private ProductRepository productRepository;
+    @Mock private ICategoryService categoryService;
+    @Mock private IUserService userService;
+    @Mock private ProductMapper productMapper;
 
-    @Test
-    void should_find_product_by_id() {
-        Product initialProduct = Product.builder()
+    private Product product;
+    private ProductDto productDto;
+    @BeforeEach
+    void setUp() {
+        product = Product.builder()
                 .id(1)
                 .name("product")
                 .price(10.0)
                 .quantity(10)
                 .build();
-        when(productRepository.findById(1)).thenReturn(Optional.ofNullable(initialProduct));
+        productDto = ProductDto.builder()
+                .id(1)
+                .name("product")
+                .price(10.0)
+                .quantity(10)
+                .categoryId(1)
+                .userId(2)
+                .build();
+    }
+
+    @Test
+    void should_find_product_by_id() {
+        when(productRepository.findById(1)).thenReturn(Optional.ofNullable(product));
         productService.findById(1);
         verify(productRepository, times(1)).findById(1);
     }
+
+    @Test
+    void testCreateProduct() {
+        // Arrange
+        Category category = Category.builder()
+                .id(1)
+                .name("category")
+                .build();
+        User user = User.builder()
+                .id(2)
+                .email("test@mail.fr")
+                .password("password")
+                .lastname("lastname")
+                .firstname("firstname")
+                .build();
+        Product mappedProduct = Product.builder()
+                .id(1)
+                .name("product")
+                .price(10.0)
+                .quantity(10)
+                .build();
+
+        when(categoryService.findById(1)).thenReturn(category);
+        when(userService.findById(2)).thenReturn(user);
+        when(productMapper.toEntity(productDto)).thenReturn(mappedProduct); // Mock ProductMapper
+        when(productRepository.save(mappedProduct)).thenReturn(mappedProduct); // Mock ProductRepository
+
+        // Act & Assert
+        assertDoesNotThrow(() -> productService.createProduct(productDto));
+
+        // Additional Verification
+        verify(categoryService, times(1)).findById(1);
+        verify(userService, times(1)).findById(2);
+        verify(productMapper, times(1)).toEntity(productDto);
+        verify(productRepository, times(1)).save(mappedProduct);
+    }
+
 }
