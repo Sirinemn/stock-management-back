e83d5149ab56add9d0c8f2171d27a2647fe15d82
Sirinemn <sirinemn@outlook.fr>
2025-03-11 21:34:20 +0100

    add ICategoryService interface

---
+++ src/main/java/fr/sirine/stock_management_back/service/ICategoryService.java
@@ -0,0 +1,15 @@
+package fr.sirine.stock_management_back.service;
+
+import fr.sirine.stock_management_back.dto.CategoryDto;
+import fr.sirine.stock_management_back.entities.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    List<CategoryDto> getAllCategories();
+    void addCategory(String categoryName);
+    void deleteCategory(Integer id);
+    Category findById(Integer categoryId);
+    CategoryDto getByName(String name);
+}
+
