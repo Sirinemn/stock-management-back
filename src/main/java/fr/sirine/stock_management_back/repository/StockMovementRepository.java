323ca7eecd2efe4af37d336a02fd7c7396a938af
Sirinemn <sirinemn@outlook.fr>
2025-03-13 15:25:21 +0100

    add methods to StockMovementRepository

--- src/main/java/fr/sirine/stock_management_back/repository/StockMovementRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/StockMovementRepository.java
@@ -4,9 +4,14 @@ import fr.sirine.stock_management_back.entities.StockMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface StockMovementRepository extends JpaRepository<StockMovement, Integer> {
     List<StockMovement> findAllByProductId(Integer productId);
+    List<StockMovement> findByUserId(Integer userId);
+    List<StockMovement> findByDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+    List<StockMovement> findByUserIdAndProductIdAndDateBetween(Integer userId, Integer productId, LocalDateTime startDate, LocalDateTime endDate);
+
 }
