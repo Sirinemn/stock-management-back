68f78d0e10cc667bbf80fad1ffbd77443fd39907
Sirinemn <sirinemn@outlook.fr>
2025-03-13 10:27:05 +0100

    add a constructor

--- src/main/java/fr/sirine/stock_management_back/dto/ProductDto.java
+++ src/main/java/fr/sirine/stock_management_back/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package fr.sirine.stock_management_back.dto;
 
+import fr.sirine.stock_management_back.entities.Product;
 import lombok.*;
 
 @Getter
@@ -17,4 +18,15 @@ public class ProductDto {
     private Integer userId;
     private Integer categoryId; // Utilisé pour les requêtes
     private String categoryName; // Utilisé pour l'affichage
+
+    public ProductDto(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.quantity = product.getQuantity();
+        this.price = product.getPrice();
+        this.userId = product.getUser().getId();
+        this.categoryId = product.getCategory().getId();
+        this.categoryName = product.getCategory().getName();
+    }
 }
