ae5e871237c7ec96f203a1edbe572001338537fa
Sirinemn <sirinemn@outlook.fr>
2025-03-13 10:20:40 +0100

    add findAllByProductId to StockMovementRepository

--- src/main/java/fr/sirine/stock_management_back/repository/StockMovementRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/StockMovementRepository.java
@@ -4,6 +4,9 @@ import fr.sirine.stock_management_back.entities.StockMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StockMovementRepository extends JpaRepository<StockMovement, Integer> {
+    List<StockMovement> findAllByProductId(Integer productId);
 }
