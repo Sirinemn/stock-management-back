5b4f2e68fdf5bf4aeecbc2e16ad78cf6b6571887
Sirinemn <sirinemn@outlook.fr>
2025-03-13 13:31:12 +0100

    update StockMovementMapper

--- src/main/java/fr/sirine/stock_management_back/mapper/StockMovementMapper.java
+++ src/main/java/fr/sirine/stock_management_back/mapper/StockMovementMapper.java
@@ -3,7 +3,9 @@ package fr.sirine.stock_management_back.mapper;
 import fr.sirine.stock_management_back.dto.StockMovementDto;
 import fr.sirine.stock_management_back.entities.Product;
 import fr.sirine.stock_management_back.entities.StockMovement;
+import fr.sirine.stock_management_back.entities.User;
 import fr.sirine.stock_management_back.service.impl.ProductService;
+import fr.sirine.stock_management_back.service.impl.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -16,15 +18,23 @@ public abstract class StockMovementMapper implements EntityMapper<StockMovementD
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private UserService userService;
 
     @Mapping(target = "productId", expression = "java(stockMovement.getProduct().getId())")
+    @Mapping(target = "userId", expression = "java(stockMovement.getUser().getId())")
     public abstract StockMovementDto toDto(StockMovement stockMovement);
 
     @Mapping(target = "product", source = "productId", qualifiedByName = "findProductById")
+    @Mapping(target = "user", source = "userId", qualifiedByName = "findUserById")
     public abstract StockMovement toEntity(StockMovementDto stockMovementDto);
 
     @Named("findProductById")
     Product findProductById(Integer productId) {
         return productService.findById(productId);
+    }
+    @Named("findUserById")
+    User findUserById(Integer userId) {
+        return userService.findById(userId);
     }
 }
