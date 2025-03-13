daacb63573f623eb5292f8fd8888e7f19b091c8e
Sirinemn <sirinemn@outlook.fr>
2025-03-13 10:44:38 +0100

    implement StockMovementController

---
+++ src/main/java/fr/sirine/stock_management_back/controller/StockMovementController.java
@@ -0,0 +1,47 @@
+package fr.sirine.stock_management_back.controller;
+
+import fr.sirine.stock_management_back.dto.StockMovementDto;
+import fr.sirine.stock_management_back.service.impl.StockMovementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/stocks")
+@Tag(name = "Stock Movements", description = "API pour la gestion des mouvements de stock")
+public class StockMovementController {
+    private final StockMovementService stockMovementService;
+
+    public StockMovementController(StockMovementService stockMovementService) {
+        this.stockMovementService = stockMovementService;
+    }
+
+    @Operation(summary = "Ajouter un mouvement de stock", description = "Ajoute une entrée ou une sortie de stock pour un produit donné.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Mouvement de stock ajouté avec succès"),
+            @ApiResponse(responseCode = "400", description = "Requête invalide ou stock insuffisant"),
+            @ApiResponse(responseCode = "404", description = "Produit non trouvé")
+    })
+    @PostMapping("/movement")
+    public ResponseEntity<StockMovementDto> addStockMovement(@RequestBody @Valid StockMovementDto stockMovementDto) {
+        StockMovementDto savedMovement = stockMovementService.addStockMovement(stockMovementDto);
+        return ResponseEntity.ok(savedMovement);
+    }
+    @Operation(summary = "Lister les mouvements d'un produit", description = "Retourne l'historique des entrées et sorties de stock pour un produit donné.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Liste des mouvements de stock récupérée avec succès"),
+            @ApiResponse(responseCode = "404", description = "Produit non trouvé")
+    })
+    @GetMapping("/movements/{productId}")
+    public ResponseEntity<List<StockMovementDto>> getStockMovements(@PathVariable Integer productId) {
+        List<StockMovementDto> movements = stockMovementService.getStockMovementsByProduct(productId);
+        return ResponseEntity.ok(movements);
+    }
+}
+
