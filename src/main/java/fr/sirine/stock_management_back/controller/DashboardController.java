02b6af7c3d6556d2899351f4761a653a0f20b9db
Sirinemn <sirinemn@outlook.fr>
2025-03-17 15:14:01 +0100

    implements DashboardController

---
+++ src/main/java/fr/sirine/stock_management_back/controller/DashboardController.java
@@ -0,0 +1,22 @@
+package fr.sirine.stock_management_back.controller;
+
+import fr.sirine.stock_management_back.dto.DashboardOverviewDto;
+import fr.sirine.stock_management_back.service.impl.DashboardService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class DashboardController {
+    private final DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+    @GetMapping("/overview")
+    public ResponseEntity<DashboardOverviewDto> getDashboardOverview() {
+        return ResponseEntity.ok(dashboardService.getDashboardOverview());
+    }
+}
