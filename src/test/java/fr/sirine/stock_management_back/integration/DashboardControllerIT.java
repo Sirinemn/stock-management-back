e159ce56502a3f16d681879425a92e93fcb38524
Sirinemn <sirinemn@outlook.fr>
2025-03-17 15:54:33 +0100

    implements DashboardControllerIT

---
+++ src/test/java/fr/sirine/stock_management_back/integration/DashboardControllerIT.java
@@ -0,0 +1,55 @@
+package fr.sirine.stock_management_back.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.sirine.stock_management_back.dto.DashboardOverviewDto;
+import fr.sirine.stock_management_back.dto.StockMovementDto;
+import fr.sirine.stock_management_back.service.impl.DashboardService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class DashboardControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private DashboardService dashboardService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void getDashboardOverview() throws Exception {
+        StockMovementDto stockMovementDto = StockMovementDto.builder()
+                .productId(1)
+                .quantity(10)
+                .build();
+        DashboardOverviewDto dashboardOverviewDto = DashboardOverviewDto.builder()
+                .lowStockProducts(5L)
+                .totalProducts(10L)
+                .recentMovements(List.of(stockMovementDto))
+                .build();
+        when(dashboardService.getDashboardOverview()).thenReturn(dashboardOverviewDto);
+        MockHttpServletRequestBuilder request = get("/dashboard/overview")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(dashboardOverviewDto));
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.lowStockProducts").value(5));
+    }
+}
