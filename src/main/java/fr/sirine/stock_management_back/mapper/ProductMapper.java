59ea923c7f826b0e30ea309d752c6b4181b2448e
Sirinemn <sirinemn@outlook.fr>
2025-03-22 15:54:48 +0100

    update ProductMapper

--- src/main/java/fr/sirine/stock_management_back/mapper/ProductMapper.java
+++ src/main/java/fr/sirine/stock_management_back/mapper/ProductMapper.java
@@ -2,9 +2,11 @@ package fr.sirine.stock_management_back.mapper;
 
 import fr.sirine.stock_management_back.dto.ProductDto;
 import fr.sirine.stock_management_back.entities.Category;
+import fr.sirine.stock_management_back.entities.Group;
 import fr.sirine.stock_management_back.entities.Product;
 import fr.sirine.stock_management_back.entities.User;
 import fr.sirine.stock_management_back.service.impl.CategoryService;
+import fr.sirine.stock_management_back.service.impl.GroupService;
 import fr.sirine.stock_management_back.service.impl.UserService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -20,14 +22,19 @@ public abstract class ProductMapper implements EntityMapper<ProductDto, Product>
     UserService userService;
     @Autowired
     private CategoryService categoryService;
+    @Autowired
+    private GroupService groupService;
 
     @Mapping(source = "user.id", target = "userId")
     @Mapping(source = "category.id", target = "categoryId")
     @Mapping(source = "category.name", target = "categoryName")
+    @Mapping(source = "user.userGroup.id", target = "groupId")
+    @Mapping(source = "user.userGroup.name", target = "groupName")
     public abstract ProductDto toDto(Product product);
 
     @Mapping(source = "userId", target = "user", qualifiedByName = "findUserById")
     @Mapping(source = "categoryId", target = "category", qualifiedByName = "findCategoryById")
+    @Mapping(source = "groupId", target = "user.userGroup", qualifiedByName = "findGroupById")
     public abstract Product toEntity(ProductDto productDto);
 
     @Named("findUserById")
@@ -37,5 +44,9 @@ public abstract class ProductMapper implements EntityMapper<ProductDto, Product>
     @Named("findCategoryById")
     Category findCategoryById(Integer categoryId) {
         return categoryService.findById(categoryId);
+    }
+    @Named("findGroupById")
+    Group findGroupById(Integer groupId) {
+        return groupService.findById(groupId);
     }
 }
