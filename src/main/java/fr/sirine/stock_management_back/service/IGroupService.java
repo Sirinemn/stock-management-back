7433e6d8f13ae04b80aa4d5a5ad5135f3d51f4d9
Sirinemn <sirinemn@outlook.fr>
2025-03-22 16:10:35 +0100

    add IGroupService

---
+++ src/main/java/fr/sirine/stock_management_back/service/IGroupService.java
@@ -0,0 +1,7 @@
+package fr.sirine.stock_management_back.service;
+
+import fr.sirine.stock_management_back.entities.Group;
+
+public interface IGroupService {
+     Group findById(Integer id);
+}
--- src/main/java/fr/sirine/stock_management_back/service/impl/GroupService.java
+++ src/main/java/fr/sirine/stock_management_back/service/impl/GroupService.java
@@ -3,10 +3,11 @@ package fr.sirine.stock_management_back.service.impl;
 import fr.sirine.stock_management_back.entities.Group;
 import fr.sirine.stock_management_back.exceptions.custom.GroupNotFountException;
 import fr.sirine.stock_management_back.repository.GroupRepository;
+import fr.sirine.stock_management_back.service.IGroupService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class GroupService {
+public class GroupService implements IGroupService {
     private final GroupRepository groupRepository;
 
     public GroupService(GroupRepository groupRepository) {
