ea8c8737629c22609de3922ebe05682ef1910287
Sirinemn <sirinemn@outlook.fr>
2025-03-22 15:24:47 +0100

    add findByNameMethod to GroupRepository

--- src/main/java/fr/sirine/stock_management_back/repository/GroupRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/GroupRepository.java
@@ -4,6 +4,9 @@ import fr.sirine.stock_management_back.entities.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
+    Optional<Group> findByName(String groupName);
 }
