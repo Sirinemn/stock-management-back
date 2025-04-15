a29b20154788079d5d4c29862433ee72fc7cfaf2
Sirinemn <sirinemn@outlook.fr>
2025-04-15 19:48:33 +0200

    add getallusers endpoint

--- src/main/java/fr/sirine/stock_management_back/controller/UserController.java
+++ src/main/java/fr/sirine/stock_management_back/controller/UserController.java
@@ -3,12 +3,17 @@ package fr.sirine.stock_management_back.controller;
 import fr.sirine.stock_management_back.dto.UserDto;
 import fr.sirine.stock_management_back.payload.response.MessageResponse;
 import fr.sirine.stock_management_back.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     private final UserService userService;
@@ -17,14 +22,21 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get user by id", description = "Get user by id")
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(Integer id) {
         return ResponseEntity.ok(userService.getById(id));
     }
+    @Operation(summary = "Delete user by id", description = "Delete user by id")
     @PutMapping("/{id}")
     public ResponseEntity<MessageResponse> updateUser(@PathVariable Integer id, @RequestBody @Valid UserDto userDto) {
         userService.updateUserById(id, userDto );
         return ResponseEntity.ok(new MessageResponse("User updated successfully!"));
+    }
+    @Operation(summary = "Get all users by admin id", description = "Get all users by admin id")
+    @GetMapping()
+    public ResponseEntity<List<UserDto>> getUsersByAdmin(@RequestParam Integer adminId) {
+        return ResponseEntity.ok(userService.getUsersByAdmin(adminId));
     }
 }
 
