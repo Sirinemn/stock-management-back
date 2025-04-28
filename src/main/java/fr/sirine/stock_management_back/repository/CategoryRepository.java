01ee6705275ef03bf63e349632ba13ffa98a5aaf
Sirinemn <sirinemn@outlook.fr>
2025-04-28 10:53:57 +0200

    add findByGroupId method to CategoryRepository

--- src/main/java/fr/sirine/stock_management_back/repository/CategoryRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/CategoryRepository.java
@@ -10,5 +10,5 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
     Category findByName(String name);
-    List<Category> findByGroup(Group group);
+    List<Category> findByGroupId(Integer groupId);
 }
