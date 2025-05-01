85cdd9c599b5ec4ab70ce30d9d610706fcc4d7a3
Sirinemn <sirinemn@outlook.fr>
2025-05-01 21:08:48 +0200

    add findByNameAndGroupId method

--- src/main/java/fr/sirine/stock_management_back/repository/ProductRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/ProductRepository.java
@@ -5,12 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findByUserId(Long userId);
 
     long countByQuantityLessThan(int i);
+
+    Optional<Product> findByNameAndGroupId(String name, Integer integer);
 
     List<Product> findAllByGroupId(Integer groupId);
 }
