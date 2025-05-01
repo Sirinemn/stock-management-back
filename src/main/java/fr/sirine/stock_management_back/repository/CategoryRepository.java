034ae1722ae9a5e18e6ebb05e4a319af43efd339
Sirinemn <sirinemn@outlook.fr>
2025-05-01 19:29:03 +0200

    add findByNameAndGroupId to CategoryRepository

--- src/main/java/fr/sirine/stock_management_back/repository/CategoryRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/CategoryRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
-    Category findByName(String name);
+    Optional<Category> findByNameAndGroupId(String name, Integer groupId);
     List<Category> findByGroupId(Integer groupId);
 }
