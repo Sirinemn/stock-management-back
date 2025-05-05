981d6e44aa0d344cdb704871b42bd782a61220f4
Sirinemn <sirinemn@outlook.fr>
2025-05-05 10:41:33 +0200

    delete unique= true in Category name

--- src/main/java/fr/sirine/stock_management_back/entities/Category.java
+++ src/main/java/fr/sirine/stock_management_back/entities/Category.java
@@ -24,7 +24,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
