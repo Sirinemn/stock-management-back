14f29be7f621ed6f96d72c6c665c27173dd79e9c
Sirinemn <sirinemn@outlook.fr>
2025-05-08 13:07:46 +0200

    add categorieController

---
+++ src/main/java/fr/sirine/stock_management_back/controller/CategorieController.java
@@ -0,0 +1,27 @@
+package fr.sirine.stock_management_back.controller;
+
+import fr.sirine.stock_management_back.dto.CategoryDto;
+import fr.sirine.stock_management_back.service.impl.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categories")
+public class CategorieController {
+    private final CategoryService categoryService;
+
+    public CategorieController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+    @Operation(summary = "Get all categories", description = "Retrieve a list of all categories")
+    public ResponseEntity<List<CategoryDto>> getAllCategories(@RequestParam("groupId") @Valid Integer groupId) {
+        List<CategoryDto> categories = categoryService.getAllCategories(groupId);
+        return ResponseEntity.ok(categories);
+    }
+}
