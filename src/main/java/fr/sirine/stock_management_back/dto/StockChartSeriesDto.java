3e11fcdcad8a8702a98495622a63ad1955c55b1f
Sirinemn <sirinemn@outlook.fr>
2025-05-15 09:43:20 +0200

    add stockChartSerieDto

--- src/main/java/fr/sirine/stock_management_back/dto/StockChartSeriesDto.java
+++ src/main/java/fr/sirine/stock_management_back/dto/StockChartSeriesDto.java
@@ -1,2 +1,17 @@
-package fr.sirine.stock_management_back.dto;public class StockChartSeriesDto {
+package fr.sirine.stock_management_back.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class StockChartSeriesDto {
+    private String name; // "Entrées" ou "Sorties"
+    private List<ChartPointDto> series;
 }
