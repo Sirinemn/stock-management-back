e27459fbf7169d5407eeb9abf4928adcae7c5918
Sirinemn <sirinemn@outlook.fr>
2025-06-03 10:47:03 +0200

    add PasswordResetToken entity

---
+++ src/main/java/fr/sirine/stock_management_back/entities/PasswordResetToken.java
@@ -0,0 +1,34 @@
+package fr.sirine.stock_management_back.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reset_token")
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    public PasswordResetToken() {}
+
+    public PasswordResetToken(String token, User user, LocalDateTime expiryDate) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isExpired() {
+        return expiryDate.isBefore(LocalDateTime.now());
+    }
+}
