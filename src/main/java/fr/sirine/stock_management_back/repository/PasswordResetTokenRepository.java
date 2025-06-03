117daf62512629864e31dfc982cac9e00e589865
Sirinemn <sirinemn@outlook.fr>
2025-06-03 10:57:28 +0200

    add findByToken in PasswordResetTokenRepository

--- src/main/java/fr/sirine/stock_management_back/repository/PasswordResetTokenRepository.java
+++ src/main/java/fr/sirine/stock_management_back/repository/PasswordResetTokenRepository.java
@@ -4,6 +4,9 @@ import fr.sirine.stock_management_back.entities.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Integer> {
+    Optional<PasswordResetToken> findByToken(String token);
 }
