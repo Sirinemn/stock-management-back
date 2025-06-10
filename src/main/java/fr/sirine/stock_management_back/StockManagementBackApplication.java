2dfd89657a17072ff7dbb5ef1cac025256dcf00b
Sirinemn <sirinemn@outlook.fr>
2025-06-10 15:17:48 +0200

    update class main

--- src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
+++ src/main/java/fr/sirine/stock_management_back/StockManagementBackApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -19,6 +20,7 @@ import java.util.Collections;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class StockManagementBackApplication {
 
 	public static void main(String[] args) {
